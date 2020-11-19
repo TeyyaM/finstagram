c6b312fc7ffe278d3ab3330df2323b9b78470441
TeyyaM <tessaamac@gmail.com>
2020-11-19 01:50:58 +0000

    Week 2 Day 2 about to fuck with humanized time

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,37 @@
+#this stuff was from before Week 2 Day 2
+# get '/' do
+#     File.read(File.join('app/views', 'index.html'))
+# end
+
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes > 60
+        "#{time_ago_in_minutes/60} hours ago"
+        #this was my personal addition
+    elsif time_ago_in_minutes == 60
+        "an hour ago"
+        #as was this
+    elsif time_ago_in_minutes == 1
+        "a minute ago"
+        #and this
+    elsif time_ago_in_minutes < 1
+        "less than a minute ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end
+end
+
 get '/' do
-    File.read(File.join('app/views', 'index.html'))
+    finstagram_post = {
+        username: "sharky_j",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.jpg",
+        time_ago_in_minutes: 15,
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "sharky_j:",
+            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+        }]
+    }
+    humanized_time_ago(finstagram_post[:time_ago_in_minutes])
 end
